fa7d61a22e9fe29bc7ba6a557e3731aa87304e9c
ElSicarius <amurat654@gmail.com>
2022-04-19 16:16:40 +0200

    Adding no headless option and switching to pcregrep for better compatibility with the regex

--- sources/crawlalllinks.py
+++ sources/crawlalllinks.py
@@ -51,13 +51,61 @@ class Handlers():
             request.headers.setdefault(k, v)
             request.headers[k] = self.new_headers[k]
         asyncio.create_task(request.continue_({
-        "headers": {
-            **request.headers,
-        }
+        "headers": request.headers,
     }))
 
+class Web_classic():
 
-class Web():
+    base_headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36"
+    }
+
+    def __init__(self, forced_headers = {}, timeout=30):
+        self.page = None
+        self.results = dict()
+        self.forced_headers = forced_headers
+        self.timeout = timeout
+        self.mime_types = self.get_mime_types()
+        self.load_headers()
+    
+    def load_headers(self):
+        self.headers = self.base_headers.update(self.forced_headers)
+    
+    def get_mime_types(self):
+        # iana mimes
+        mimes_csv = requests.get("https://www.iana.org/assignments/media-types/application.csv").text
+        mimes = mimes_csv.split("\n")
+        mimes = [m.split(",")[1].strip() for m in mimes if len(m.split(",")) > 1]
+
+        # custom mimes
+        mimes2_csv = requests.get("https://gist.githubusercontent.com/electerious/3d5a31a73cfd6423f835c074ab25fc06/raw/d48b8410e9aa6746cfd946bca21a1bb54c351c4e/Caddyfile").text
+        mimes2 = re.findall("[\w\d\+\-\_]+/[\w\d\+\-\_]+", mimes2_csv)
+        mimes.extend(mimes2)
+
+        # personal mimes
+        mimes3 = ["text/javascript", "text/xml"]
+        mimes.extend(mimes3)
+
+        return mimes
+    
+    def start_browser(self,):
+        self.browser = requests.Session()
+    
+    def page_goto(self, id, url):
+        try:
+            self.page = requests.get(url, timeout=self.timeout, headers=self.headers)
+        except Exception as e:
+            logger.error(f"Error: {e}")
+            return False
+        self.results["url"] = url
+        return self.page
+    
+    def get_page_content(self, id):
+        body = self.page.text
+        self.results["body"] = body
+        return body
+
+class Web_headless():
     def __init__(self, forced_headers = {}, 
             timeout=30,
             pyppeteer_args={"headless": True, "ignoreHTTPSErrors": True, "handleSIGTERM": False, "handleSIGINT": False, "executablePath": "/usr/bin/chromium-browser", "devtools": False, "args": ["--no-sandbox"]},):
@@ -134,10 +182,27 @@ class Crawl():
         self.queue.add(url)
     
     def find_all_links_v2(self, id_):
-        # uses grep to find all links in the saved page, as python's "re" is very slow (because of the [^&,\s] at the end of the regex)
-        return check_output(f"grep -Pao {findAllLinksv2} /tmp/crawlalllinks/{id_}.txt | sed \"s/[\\\"|']//g\"  | sort -uV", shell=True, executable='/bin/bash').decode("utf-8")
+        # uses pcregrep to find all links in the saved page, as python's "re" is very slow (because of the [^&,\s] at the end of the regex)
+        # we set bit limits to prevent error while grepping links
+        return check_output(f"pcregrep --match-limit 1000000000 --buffer-size 10000000 --recursion-limit 1000000000 -ao {findAllLinksv2} /tmp/crawlalllinks/{id_}.txt | sed \"s/[\\\"|']//g\"  | sort -uV", shell=True, executable='/bin/bash').decode("utf-8")
     
-    async def visit_n_parse(self, id_, url):
+    def visit_n_parse_classic(self, id_, url):
+        p = self.web.page_goto(id_, url)
+        if not p:
+            self.add_failed({url})
+        else:
+            self.add_visited({url}, status=p.status_code, headers=p.headers)
+        self.queue.discard(url)
+        content = self.web.get_page_content(id_)
+        self.results_pages[url]["len"] = len(content)
+        with open(f"/tmp/crawlalllinks/{id_}.txt", "w") as f:
+            f.write(content)
+        links = self.find_all_links_v2(id_)
+        for link in links.splitlines():
+            retrieved_links.add(link)
+        return set(links.splitlines())
+    
+    async def visit_n_parse_headless(self, id_, url):
         p = await self.web.page_goto(id_, url)
         if not p:
             self.add_failed({url})
@@ -253,8 +318,8 @@ def load_headers(headers_string):
         headers[header_name] = value
     return headers
 
-async def main(args):
-    veb = Web(load_headers(args.header), timeout=args.timeout)
+async def main_headless(args):
+    veb = Web_headless(load_headers(args.header), timeout=args.timeout)
     crawler = Crawl(web=veb, url=args.url)
 
     init_url_parsed = urlparse(args.url)
@@ -273,7 +338,7 @@ async def main(args):
     signal.signal(signal.SIGINT, signal_handler)
 
     id_ = await veb.new_page()
-    links = await crawler.visit_n_parse(id_, crawler.init_url)
+    links = await crawler.visit_n_parse_headless(id_, crawler.init_url)
     
     def get_next_urls(links):
         urls = set()
@@ -313,7 +378,7 @@ async def main(args):
         url = crawler.queue.pop()
         logger.debug(f"Next page to crawl: {url}")
 
-        links = await crawler.visit_n_parse(id_, url)
+        links = await crawler.visit_n_parse_headless(id_, url)
         crawler.add_queue(get_next_urls(links))
         crawler.print_status()
     
@@ -325,6 +390,77 @@ async def main(args):
     print("================= results ===================")
     print(crawler.return_results_formatted())
 
+def main_classic(args):
+    veb = Web_classic(forced_headers=load_headers(args.header), timeout=args.timeout)
+    crawler = Crawl(web=veb, url=args.url)
+
+    init_url_parsed = urlparse(args.url)
+
+    def signal_handler(sig, frame):
+        logger.warning(f"Caught ctrl+c, saving results...")
+        crawler.write_results()
+        print("================= links ===================")
+        print("\n".join(sorted(retrieved_links)))
+        print("================= results ===================")
+        print(crawler.return_results_formatted())
+        
+        exit(1)
+        
+    signal.signal(signal.SIGINT, signal_handler)
+
+    id_ = 0
+    links = crawler.visit_n_parse_classic(id_, crawler.init_url)
+    
+    def get_next_urls(links):
+        urls = set()
+        for link in links:
+            link = link.strip()
+            if link in veb.mime_types:
+                # logger.warning(f"Link looks like a mime type: {link}, ignoring")
+                continue
+            
+            link_ = Link(link, crawler.init_url)
+            type_of_link = link_.get_link_type()
+            if type_of_link == "unknown":
+                # logger.warning(f"Link type is unknown: {link}, ignoring")
+                continue
+            
+            full_url = link_.format_link()
+
+            if link_.has_garbage_extension():
+                logger.warning(f"Link has a garbage extension: {link}, ignoring")
+                # adding to visited to keep track of links that are not useful
+                crawler.visited.add(full_url)
+                continue
+
+            # print(f"{link} - {type_of_link} - {full_url}")
+
+            if args.mode == "sub" and ".".join(urlparse(full_url).netloc.split(".")[-2:]) == ".".join(init_url_parsed.netloc.split(".")[-2:]):
+                urls.add(full_url)
+            if args.mode == "strict" and urlparse(full_url).netloc == init_url_parsed.netloc:
+                urls.add(full_url)
+            if args.mode == "lax":
+                urls.add(full_url)
+        return urls
+
+    next_urls = get_next_urls(links)
+    crawler.add_queue(next_urls)
+    logger.debug("Init page crawled, crawling subpages now")
+    while len(crawler.queue) > 0 and len(crawler.fails) < args.max_fails and len(crawler.visited | crawler.fails) < args.max_visits:
+        url = crawler.queue.pop()
+        logger.debug(f"Next page to crawl: {url}")
+
+        links = crawler.visit_n_parse_classic(id_, url)
+        crawler.add_queue(get_next_urls(links))
+        crawler.print_status()
+    
+    crawler.write_results()
+    print("================= links ===================")
+    print("\n".join(sorted(retrieved_links)))
+    print("================= results ===================")
+    print(crawler.return_results_formatted())
+
+
 def get_arguments():
     parser = argparse.ArgumentParser(description="Crawl all links from a webpage")
     parser.add_argument("url", type=str, help="URL to crawl")
@@ -333,6 +469,7 @@ def get_arguments():
     parser.add_argument("-m", "--mode", type=str, help="Crawling mode", default="sub", choices=["sub", "lax", "strict"])
     parser.add_argument("-H", "--header", type=str, help="Headers to send", action='append', default=[])
     parser.add_argument("--timeout", type=int, help="Timeout for fetching web page", default=35)
+    parser.add_argument("--headless", help="Use a headless browser to run the crawler", action="store_true", default=False)
 
     return parser.parse_args()
 
@@ -342,4 +479,7 @@ if __name__ == "__main__":
     if not os.path.exists("/tmp/crawlalllinks"):
         os.mkdir("/tmp/crawlalllinks")
     args = get_arguments()
-    asyncio.get_event_loop().run_until_complete(main(args))
+    if args.headless:
+        asyncio.get_event_loop().run_until_complete(main_headless(args))
+    else:
+        main_classic(args)
